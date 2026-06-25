44068bfe13c96f10194ad3d25ae2400fc3d8fb26
Jürgen Willi Sievers <JSievers@NadiSoft.de>
2026-06-25 19:46:55 +0200

    Add JSON based R-Net message broker v0.2.1

--- RNetMsgBroker_de_DE.ts
+++ RNetMsgBroker_de_DE.ts
@@ -1,3 +1,19 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de_DE"></TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>RNetMsgBroker</name>
+    <message>
+        <source>Kann JSON-Datei nicht öffnen: %1</source>
+        <translation>Kann JSON-Datei nicht öffnen: %1</translation>
+    </message>
+    <message>
+        <source>JSON-Parsefehler bei Offset %1: %2</source>
+        <translation>JSON-Parsefehler bei Offset %1: %2</translation>
+    </message>
+    <message>
+        <source>JSON enthält keine Frame-Objekte</source>
+        <translation>JSON enthält keine Frame-Objekte</translation>
+    </message>
+</context>
+</TS>
